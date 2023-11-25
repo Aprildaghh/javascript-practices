5e8f4394c0395bbbe5e52f41ff6bb9264dbfac5a
Abdullah Aslan <abdullah.aslan.se@gmail.com>
2023-11-25 03:38:12 +0300

    added localStorage

---
+++ localStorage/script.js
@@ -0,0 +1,15 @@
+
+// there are 5 functions of local storage
+// getItem()
+// setItem()
+// removeItem()
+// clear()
+// key()
+
+// to get to local storage on browser simply
+// get to application->storage->local storage
+
+window.localStorage.setItem("name", "abdullah");
+
+console.log(window.localStorage.getItem("name"));
+
